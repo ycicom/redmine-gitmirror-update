d3187239ce128fa0d5e1e70d8ecb29558a09c498
ycicom <thaolt@songphi.com>
2011-11-17 11:20:32 +0700

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_github_hook do
-  name 'Redmine Github Hook plugin'
-  author 'Jakob Skjerning'
-  description 'This plugin allows your Redmine installation to receive Github post-receive notifications'
+Redmine::Plugin.register :redmine_gitmirror_hook do
+  name 'Redmine Git Mirror Update plugin'
+  author 'Thao Le Thach'
+  description 'This plugin allows your Redmine installation to update mirror of repository and fetch changesets'
   version '0.1.1'
 end
